7ec11f9d32e6e907d323e839f8eede9ffe5b6ccf
kimds777 <77198242+kimds777@users.noreply.github.com>
2023-04-09 16:13:25 +0900

    20230409 백준공부

---
+++ bj_git/b9498.java
@@ -0,0 +1,19 @@
+package bj_git;
+
+import java.util.Scanner;
+
+public class b9498 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int a = sc.nextInt()/10;
+		sc.close();
+		switch (a) {
+		case 10: case 9:System.out.println("A"); break;
+		case 8:System.out.println("B"); break;
+		case 7:System.out.println("C"); break;
+		case 6:System.out.println("D"); break;
+		default: System.out.println("F");
+		}
+		
+	}
+}
