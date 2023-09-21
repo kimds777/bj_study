5c0b71ccf5aa6762c8b8293eff06763f0b440621
kimds777 <77198242+kimds777@users.noreply.github.com>
2023-09-21 22:57:11 +0900

    20230921 백준공부

---
+++ bj_git/b1157.java
@@ -0,0 +1,36 @@
+package bj_git;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b1157 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine().toUpperCase();
+		br.close();
+
+		int[] cnt = new int[26];
+
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = 0; j < s.length(); j++) {
+				if (i != j && s.charAt(i) == s.charAt(j)) {
+					cnt[s.charAt(i) - 65]++;
+				}
+			}
+		}
+
+		int max = 0;
+		char ch = '?';
+		for (int i = 0; i < 26; i++) {
+			if (cnt[i] > max) {
+				max = cnt[i];
+				ch = (char) (i + 65);
+			} else if (cnt[i] == max) {
+				ch = '?';
+			}
+		}
+
+		System.out.println(ch);
+	}
+}
