0cd3c8bf04ea54afab555b62c5aec4b38c649f89
kimds777 <77198242+kimds777@users.noreply.github.com>
2023-11-01 21:18:40 +0900

    20231101 백준공부

--- bj_git/b1085.java
+++ bj_git/b1085.java
@@ -1,7 +1,22 @@
 package bj_git;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class b1085 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] rec = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		br.close();
 		
+		List<Integer> length = Arrays.asList(rec[0],rec[1],rec[2]-rec[0],rec[3]-rec[1]);
+		Collections.sort(length);
+		
+		System.out.println(length.get(0));		
 	}
 }
---
+++ bj_git/b3009.java
@@ -0,0 +1,10 @@
+package bj_git;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class b3009 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
