3498a354128040d282991430cf80b2a010ab4348
kimds777 <77198242+kimds777@users.noreply.github.com>
2023-11-18 13:49:13 +0900

    20231118 백준공부

---
+++ bj_git/b24262.java
@@ -0,0 +1,16 @@
+package bj_git;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b24262 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		br.close();
+		
+		System.out.println(1);
+		System.out.println(0);
+	}
+}
