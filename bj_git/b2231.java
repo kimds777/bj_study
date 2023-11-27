6da07ca4bd855f8563d8b5c565a236a700fbfea9
kimds777 <77198242+kimds777@users.noreply.github.com>
2023-11-27 16:54:23 +0900

    20231127 백준공부

--- bj_git/b2231.java
+++ bj_git/b2231.java
@@ -4,43 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class b2231 { //수정해야함
+public class b2231 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		br.close();
 
-		int maxMinus = 0, temp, minus, ans = N;
-		while (true) {
-			temp = ans - maxMinus;
-			minus = 0;
+		int ans = 0;
+		for (int i = 1; i < N; i++) {
+			int number = i;
 
-			while (temp > 0) {
-				System.out.println(temp);
-				minus += temp % 10;
-				temp /= 10;
+			int modSum = 0;
+			while (number > 0) {
+				modSum += number % 10;
+				number /= 10;
 			}
 
-			if (maxMinus + minus > 27) {
+			if (modSum + i == N) {
+				ans = i;
 				break;
 			}
-
-			maxMinus += minus;
-			int ansTemp = ans - maxMinus;
-			int ansSum = 0;
-
-			while (ansTemp > 0) {
-				ansSum += ansTemp % 10;
-				ansTemp /= 10;
-			}
-
-			if (ansSum == minus) {
-				ans = ansTemp;
-			}
-		}
-
-		if (N == ans) {
-			ans = 0;
 		}
 
 		System.out.println(ans);
