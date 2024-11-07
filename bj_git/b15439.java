c2535a7416dfa3593a96b125d2182d8c9b32aa58
Daseul Kim <tina27849@gmail.com>
2024-11-07 12:05:24 +0900

    Feat : 20241107 15439번 베라의 패션

---
+++ bj_git/b15439.java
@@ -0,0 +1,14 @@
+package bj_git;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b15439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        System.out.println(N * (N - 1));
+    }
+}
