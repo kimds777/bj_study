9a8ff405d06023f223e9168026895822857110a2
Daseul Kim <tina27849@gmail.com>
2024-12-09 11:46:34 +0900

    Feat : 20241209 1037번 약수

---
+++ bj_git/b1037.java
@@ -0,0 +1,17 @@
+package bj_git;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class b1037 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(A[0] * A[A.length - 1]);
+    }
+}
